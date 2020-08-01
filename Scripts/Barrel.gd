08c23f28a1979b184b9cb91e77c8bb05f2338c09
Lyle Olsen <lyle3olsen@outlook.com>
2020-08-01 08:42:13 -0600

    found the barrel effect bug and fixed it.

--- Scripts/Barrel.gd
+++ Scripts/Barrel.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const BarrelEffect = preload("res://Scenes/BarrelEffect.tscn")
+onready var hurtbox = $Hurtbox
 
 func create_barrel_effect():
 	var barrelEffect = BarrelEffect.instance()
@@ -8,5 +9,6 @@ func create_barrel_effect():
 	barrelEffect.global_position = global_position
 
 func _on_Hurtbox_area_entered(area):
+	hurtbox.create_hit_effect()
 	create_barrel_effect()
 	queue_free()
