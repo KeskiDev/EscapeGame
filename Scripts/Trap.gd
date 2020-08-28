9e34949b6ff5f7592d9c840ccc010960948ecc79
Lyle Olsen <lyle3olsen@outlook.com>
2020-08-28 07:50:56 -0600

    began work on the house traps

---
+++ Scripts/Trap.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+#preload barrel, vase, table2, crate
+#something you can destroy to escape other you can't.
+# make sure there is always a way to get out of the room without killing the ghosts
+
+
+
+func _on_Trap_body_exited(body):
+	#spawn a random decoration to block where the trap is
+	pass # Replace with function body.
