e7738100f99685bca3619f9ba42f87783ea1f093
Lyle Olsen <lyle3olsen@outlook.com>
2020-09-05 15:55:03 -0600

    work on level 3, adding walls and player

---
+++ Scenes/Level3/Level3.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+func _ready():
+	Global.currentScenePath = "res://Scenes/Level3/Level3.tscn"
+	Global.currentLevelNumber = 3
+	Global.nextLevelScenePath = ""
+	Global.newFastestTime = false
+	PlayerStats.health = 4
+	PlayerStats.skull_count = 0
+
+
+func _on_levelCleared_body_entered(body):
+	if body.is_in_group("player"):
+		var playerTime = get_node("CanvasLayer/Countdown/Label").text
+		Global.endPlayerTime = playerTime
+		Global.summaryMessage = "You made it out in: "
+		
+		get_tree().change_scene("res://Scenes/Menu Screens/Summary.tscn")
+
+
+func _on_doorClosedNotify_body_entered(body):
+	if body.is_in_group("player"):
+		var levelNode = get_node("CanvasLayer/PlayerMessage/Label")
+		levelNode.text = "The way is shut by the dead. Find the skulls!"
+
+
+func _on_doorClosedNotify_body_exited(body):
+	if body.is_in_group("player"):
+		var levelNode = get_node("CanvasLayer/PlayerMessage/Label")
+		levelNode.text = ""
